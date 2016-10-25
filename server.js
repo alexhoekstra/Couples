4887ba028508f2c5a7f92fe0a0d7d82541b985d3
alexhoekstra <aph4@students.calvin.edu>
2016-10-24 20:35:12 -0400

    testing REST

--- server.js
+++ server.js
@@ -2,13 +2,128 @@ var http = require("http");
 var fs = require("fs");
 var path = require("path");
 var mime = require("mime");
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var NAME_COLLECTION = "names";
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/names"
+ *    GET: finds all names
+ *    POST: creates a new name
+ */
+
+app.get("/names", function(req, res) {
+	db.collection(NAME_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get Names.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/names", function(req, res) {
+  var newName = req.body;
+  newName.createDate = new Date();
+
+  if (!(req.body.firstName)) {
+    handleError(res, "Invalid user input", "Must provide a first name.", 400);
+  }
+
+  db.collection(NAME_COLLECTION).insertOne(newName, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new Name.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+/*  "/names/:id"
+ *    GET: find name by id
+ *    PUT: update name by id
+ *    DELETE: deletes name by id
+ */
+
+app.get("/names/:id", function(req, res) {
+	db.collection(NAME_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contact");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+app.put("/names/:id", function(req, res) {
+	var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(NAME_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update contact");
+    } else {
+      res.status(204).end();
+    }
+  });
+});
+
+app.delete("/names/:id", function(req, res) {
+	db.collection(NAME_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete contact");
+    } else {
+      res.status(204).end();
+    }
+  });
+});
+
+
+
+
+
 
 /*http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("It's alive!");
   response.end();
 }).listen(3000);
-*/
+
 
 function send404(response) {
   response.writeHead(404, {"Content-type" : "text/plain"});
@@ -51,5 +166,7 @@ var server = http.createServer(function(request, response) {
 });
 	
 var port_number = server.listen(process.env.PORT || 3000);
+
+*/
 
 
